9d823dd54b142222c1656bd20522e5e3785eea27
Leonardo Achá <leonardoacha@hotmail.com>
2023-05-24 10:11:51 -0400

    generar_programa_gestion documented

--- generar_programa_gestion.py
+++ generar_programa_gestion.py
@@ -10,6 +10,14 @@ openai.api_key = os.environ['SYS_OPENIA_API_KEY']
 
 class gestion_program(pd.DataFrame):
     def __init__(self, IPER):
+        """
+        Crea un programa de gestión a partir de los datos de la IPER.
+
+        Argumentos:
+        
+        - IPER: DataFrame que contiene los datos del la Matriz IPER.
+
+        """
         peligros = IPER['PELIGRO_x000D_\n_x000D_\n(Evento Peligroso, categorias de Lista Maestra OST-SGI.SI.001)'].unique()
         sectores = IPER['SECTOR / AREA / UNIDAD / PROCESO O SUB-PROCESO'].unique()
         
@@ -36,15 +44,25 @@ class gestion_program(pd.DataFrame):
 
                 self.loc[len(self)] = [', '.join(map(str, indices)), objetivo, peligro, sector, ', '.join(map(str, subsectores)), "100 % de Actividades Ejecutadas", actividad, '', '', '', '', '0.00']
         
-        # Replace empty strings with NaN
+        # Reemplazar cadenas vacías por NaN
         self['# PLANILLA IDENTIFICACIÓN  Y EVALUACIÓN'].replace('', np.nan, inplace=True)
         self['ACTIVIDAD / TAREA / LUGAR / EQUIPO / EVENTO'].replace('', np.nan, inplace=True)
 
-        # Drop rows where '# PLANILLA IDENTIFICACIÓN  Y EVALUACIÓN' column is empty
+        # Eliminar filas donde la columna '# PLANILLA IDENTIFICACIÓN  Y EVALUACIÓN' esté vacía
         self.dropna(subset=['# PLANILLA IDENTIFICACIÓN  Y EVALUACIÓN'], inplace=True)
         self.dropna(subset=['ACTIVIDAD / TAREA / LUGAR / EQUIPO / EVENTO'], inplace=True)
 
     def generate_objective(self, peligro):
+        """
+        Genera un objetivo de salud y seguridad en el trabajo basado en el peligro dado.
+
+        Argumentos:
+        - peligro: Peligro específico.
+
+        Retorna:
+        - Objetivo generado.
+
+        """
         # Llamar a la API de OpenAI para generar un objetivo conciso para el peligro dado
         prompt = f"Genera un objetivo de salud y seguridad en el trabajo en máximo 5 palabras que resuelva el peligro: {peligro}. \nNo empezar el texto con saltos de linea, signos de putnuación ni similar."
         response = openai.Completion.create(
@@ -60,6 +78,20 @@ class gestion_program(pd.DataFrame):
         return objective
 
     def generate_activity(self, objetivo, sector, subsectores, peligro):
+        """
+        Genera una actividad enfocada en resolver los peligros en un sector específico, 
+        especificando los subsectores involucrados.
+
+        Argumentos:
+        - objetivo: Objetivo de salud y seguridad en el trabajo.
+        - sector: Sector específico.
+        - subsectores: Lista de subsectores involucrados.
+        - peligro: Peligro relacionado.
+
+        Retorna:
+        - Actividad generada.
+
+        """
         # Llamar a la API de OpenAI para generar una actividad enfocada en resolver 
         # los peligros en ese sector especificando los subsectores de cada indice involucrado.
         prompt = f"Genera en pocas palabras, el resumen de una actividad enfocada en resolver los peligros en el sector {sector}, especificando los subsectores: {', '.join(subsectores).lower()}, para alcanzar el objetivo: {objetivo}. Peligro relacionado: {peligro}. \nNo empezar el texto con saltos de linea, signos de putnuación ni similar."
@@ -76,20 +108,20 @@ class gestion_program(pd.DataFrame):
         return activity
 
 if __name__ == '__main__':  
-    # Read the Excel file
+    # Leer el archivo Excel
     file_path = 'IPER_example.xlsx'
     iper = pd.read_excel(file_path)
 
-    #Prepare file
-    # Drop rows with NaN values
+    # Preparar el archivo
+    # Eliminar filas con valores NaN
     iper = iper.dropna()
     iper = iper.set_index('#', drop=True)
     iper = iper.head()
 
-    # Create an instance of gestion_program
+    # Crear una instancia de gestion_program
     program = gestion_program(iper)
 
-    # Save the instance to an Excel file
+    # Guardar la instancia en un archivo Excel
     filename = 'programa_de_gestion.xlsx'
     program.to_excel(filename, index=False)
     print(f"Programa de gestión guardado como {filename}")
